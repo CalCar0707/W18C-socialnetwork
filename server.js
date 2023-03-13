4d32a72ae541558b0f1157e98345813a324fc52d
Call Carl <callienicole0419@gmail.com>
2023-03-13 17:04:30 -0500

    updated server.js with mongodb connection

--- server.js
+++ server.js
@@ -5,3 +5,23 @@ const mongodb = require('mongodb').MongoClient;
 const app = express();
 const port = 3001;
 
+
+// Connection string to local instance of MongoDB including database name-- INS#ERT DB NAME
+const connectionStringURI = `mongodb://127.0.0.1:27017/`;
+
+//DEclare variable to hold connection
+let db;
+
+mongodb.connect(
+    connectionStringURI,
+    // Sets connection string parser and Server Discover and Monitoring engine to true and avoids warning
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (err, client) => {
+      // Use client.db() constructor to add new db instance
+      db = client.db();
+      app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`);
+      });
+    }
+  );
+  
