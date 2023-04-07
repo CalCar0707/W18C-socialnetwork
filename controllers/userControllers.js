4a3d3bb86a6828b7bc3c1a213e1d0dbd864b0168
Call Carl <callienicole0419@gmail.com>
2023-04-07 18:11:45 -0500

    updated usercontrollers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,35 +1,34 @@
 const router = require('express').Router();
 const { User, Thought } = require('../../models');
 
-// The 'api/users' endpoint
-
-//Get all users
-router.get('/users', (req, res) => {
-    User.findAll({
-
-    }).then((userData) => {
+// The '/users' endpoint
+module.exports = {
+    //Get all users
+    getUsers(req, res) {
+        User.find({})
+        .then((userData) => {
         res.json(userData);
     });
-});
+},
 
 //Get a single user by _id and popluated thought and friend data
-router.get('/users/:id', (req, res) => {
+ getSingleUser(req, res) {
     User.findOne({}), (err, result) => {
         if (result) {
             res.status(200).json(result);
         } else {
             console.log('Uh oh, something went wrong');
             res.status(500).json({ error: 'Something went wrong' });
-        },
+        }
     };
-});
+},
 
 //Post a new user
 
 //Put to update a user by _id
 
 //Delete to remove a user by _id
-router.delete('/delete/:id', (req, res) => {
+deleteUser(req, res) {
     User.findOneAndDelete({},
         (err, result) => {
             if (result) {
@@ -42,4 +41,5 @@ router.delete('/delete/:id', (req, res) => {
         }
     )
     //db.collection().deleteOne({_id: ObjectId(req.params.id) });
-});
+}
+}
